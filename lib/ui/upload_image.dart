867aeaa46fd35065236b59571e2735e7407d9e6d
Umer Farooq <03-135202-015@student.bahria.edu.pk>
2023-12-07 17:53:52 +0500

    uploadimage

--- lib/ui/upload_image.dart
+++ lib/ui/upload_image.dart
@@ -1,50 +1,37 @@
 import 'dart:io';
-import 'dart:io' show Platform;
-import 'package:firebase_app/widgets/rouded_button.dart';
-import 'package:flutter/material.dart';
-
-// Import either the original image_picker or the web version
-// based on the platform.
-import 'package:image_picker/image_picker.dart'
-    if (dart.library.html) 'package:image_picker_web/image_picker_web.dart';
 
 import 'package:firebase_app/utils/utils.dart';
+import 'package:firebase_app/widgets/rouded_button.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_picker_web/image_picker_web.dart';
 
 class UploadImageScreen extends StatefulWidget {
-  const UploadImageScreen({Key? key}) : super(key: key);
+  const UploadImageScreen({super.key});
 
   @override
-  _UploadImageScreenState createState() => _UploadImageScreenState();
+  State<UploadImageScreen> createState() => _UploadImageScreenState();
 }
 
 class _UploadImageScreenState extends State<UploadImageScreen> {
   bool loading = false;
 
   File? _image;
-  late final ImagePicker _picker;
+  final picker = ImagePicker();
 
-  @override
-  void initState() {
-    super.initState();
-    _picker = ImagePicker();
-  }
-
-  Future<void> getGalleryImage() async {
-    final pickedImage = await _picker.pickImage(
-      source: ImageSource.gallery,
-      imageQuality: 80,
-    );
-
+  Future getGalleryImage() async {
+    final pickedImage =
+        await picker.pickImage(source: ImageSource.gallery, imageQuality: 80);
     setState(() {
       loading = true;
       if (pickedImage != null) {
         _image = File(pickedImage.path);
-        print("hello this newpath " + _image!.path);
+        print("heelo this newpath " + _image!.path.toString());
         loading = false;
       } else {
         loading = false;
         Utils().toastMessage('Pick Image from gallery ', Colors.red);
-        print("hello" + _image!.path);
+        print("heelo" + _image!.path.toString());
       }
     });
   }
@@ -70,12 +57,11 @@ class _UploadImageScreenState extends State<UploadImageScreen> {
               height: 200,
               width: 200,
               decoration: BoxDecoration(
-                color: Colors.deepPurple,
-                border: Border.all(
-                  width: 2.5,
-                  color: Colors.black,
-                ),
-              ),
+                  color: Colors.deepPurple,
+                  border: Border.all(
+                    width: 2.5,
+                    color: Colors.black,
+                  )),
               child: _image != null
                   ? Image.file(_image!.absolute)
                   : const Icon(
@@ -89,12 +75,11 @@ class _UploadImageScreenState extends State<UploadImageScreen> {
             height: 30,
           ),
           RoundedButton(
-            title: 'Upload Image',
-            onTap: () {
-              getGalleryImage();
-            },
-            loading: loading,
-          ),
+              title: 'Upload Image',
+              onTap: () {
+                getGalleryImage();
+              },
+              loading: loading)
         ],
       ),
     );
